80881c915a1ddd73d723e329219a306a64317532
Nicholas Smith <npsmith1990@gmail.com>
2016-01-11 12:20:33 -0600

    Create NSManagedObject for recipe model

---
+++ Recipe.swift
@@ -0,0 +1,17 @@
+//
+//  Recipe.swift
+//  Recipes
+//
+//  Created by Nick on 1/11/16.
+//  Copyright © 2016 Nick. All rights reserved.
+//
+
+import Foundation
+import CoreData
+
+
+class Recipe: NSManagedObject {
+
+// Insert code here to add functionality to your managed object subclass
+
+}
